72ac0432e2d3150dee51eccc0b0829b42ed68004
Luca G. Soave <luca.soave@gmail.com>
2017-09-16 21:35:38 +0200

    styling vuetify inspired by Academind's podcast 3: https://www.youtube.com/watch?v=NMFoyAnWkOE&list=PL55RiY5tL51qxUbODJG9cgrsVd7ZHbPrt&index=4

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,77 +1,40 @@
 <template>
-  <v-app light>
+  <v-app id="example-1" toolbar footer>
     <v-navigation-drawer
       persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
       v-model="drawer"
-      enable-resize-watcher
+      light
+      absolute
     >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-        >
+      <v-list dense>
+        <v-list-tile @click="">
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <v-icon>home</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title>Home</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
+    <v-toolbar class="red" dark fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Toolbar</v-toolbar-title>
+        <v-spacer></v-spacer>
+          <v-toolbar-items class="hidden-xs-only">
+            <v-btn flat>
+              <v-icon left>home</v-icon>
+              Home
+            </v-btn flat>
+          </v-toolbar-items>
     </v-toolbar>
     <main>
       <v-container fluid>
-        <nuxt />
+        <!--v-router-->
       </v-container>
     </main>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed">
-      <span>&copy; 2017</span>
+    <v-footer class="red">
+      <span class="white--text">© 2017</span>
     </v-footer>
   </v-app>
 </template>
@@ -80,17 +43,7 @@
   export default {
     data () {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { to: '/', title: 'Welcome', icon: 'apps' },
-          { to: '/inspire', title: 'Inspire', icon: 'bubble_chart' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        drawer: false
       }
     }
   }
