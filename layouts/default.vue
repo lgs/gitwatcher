906f1bab7f18fff82abba510372815a34ba38f36
Luca G. Soave <luca.soave@gmail.com>
2017-09-16 23:30:19 +0200

    complete navigation toolbar with routing inspired by 5th Academind's podcast: https://www.youtube.com/watch?v=e4M2MvU5QuQ&index=5&list=PL55RiY5tL51qxUbODJG9cgrsVd7ZHbPrt

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,32 +5,34 @@
       v-model="drawer"
       light
       absolute
+      temporary
     >
       <v-list dense>
-        <v-list-tile @click="">
+        <v-list-tile @click="" v-for="item in menuItems" :key="item.title" router :to='item.link'>
           <v-list-tile-action>
-            <v-icon>home</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Home</v-list-tile-title>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar class="red" dark fixed>
       <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title>Toolbar</v-toolbar-title>
+      <v-toolbar-title><router-link to='/' tag='span' style='cursor: pointer'>Toolbar</router-link></v-toolbar-title>
         <v-spacer></v-spacer>
           <v-toolbar-items class="hidden-xs-only">
-            <v-btn flat>
-              <v-icon left>home</v-icon>
-              Home
+            <v-btn flat v-for="item in menuItems" :key="item.title" router :to='item.link'>
+              <v-icon left>{{ item.icon }}</v-icon>
+              {{ item.title }}
             </v-btn flat>
           </v-toolbar-items>
     </v-toolbar>
     <main>
       <v-container fluid>
         <!--v-router-->
+        <router-view></router-view>
       </v-container>
     </main>
     <v-footer class="red">
@@ -43,7 +45,13 @@
   export default {
     data () {
       return {
-        drawer: false
+        drawer: false,
+        menuItems: [
+          { icon: 'home', title: 'Home', link: '/' },
+          { icon: 'info_outline', title: 'About', link: '/about' },
+          { icon: 'face', title: 'Sign Up', link: '/signup' },
+          { icon: 'lock_open', title: 'Sign In', link: 'signin' }
+        ]
       }
     }
   }
